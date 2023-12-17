e9b471db18c021063cf6cbe2861b6642c87cbc01
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-12-17 15:59:16 +0100

    completed first day

---
+++ day1/bandnamegenerator.py
@@ -0,0 +1,4 @@
+# Prompts user for the name of thier city and name of pet to generate a bamd name
+city = input("What city do you live in? \n")
+pet = input("What is the name of your pet? \n")
+print(f"Your band name could be {city} {pet}")
