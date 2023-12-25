c410367710df98170a426106c587a5e1366c9259
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-12-25 11:41:57 +0100

    checks for leap year

---
+++ day3/check_leap_year.py
@@ -0,0 +1,13 @@
+# Check if the given year is leap year or not
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
