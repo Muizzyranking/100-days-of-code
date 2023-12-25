dd0f0172a6f3eb4ac567a20ca6f96ecb30dad3fc
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-12-25 10:59:29 +0100

    checks if a number is even or odd

---
+++ day3/even_or_odd.py
@@ -0,0 +1,7 @@
+# Checks if a number is even or odd
+number = int(input("Enter a number to check if it's even or odd: "))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
