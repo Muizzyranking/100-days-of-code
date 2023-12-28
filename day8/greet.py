fc7cac12c6728c9e321ad721a31a8bd0ef817f7d
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-12-28 14:50:57 +0100

    soeme test functions

---
+++ day8/greet.py
@@ -0,0 +1,28 @@
+# simple function
+def greet():
+    print("Hello")
+    print("How are you?")
+    print("Happy coding!")
+
+
+greet()
+
+
+# function with parameter
+def greet_with(name):
+    print(f"Hello {name}")
+    print(f"How're you doing {name}?")
+    print("Happy coding!")
+
+
+greet_with("Muizzy")
+
+
+# function with 2 parameters
+def greet_with2(name, location):
+    print(f"Hello {name}")
+    print(f"What's up {location}?")
+    print("Happy coding!")
+
+
+greet_with2("Muizzy", "London")
