490d5efdb215edf1244f6a73ec82ce965b0dc79a
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-12-30 12:12:47 +0100

    calculator

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,3 +1,10 @@
+import os
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -16,12 +23,31 @@ def mul(n1, n2):
 
 operations = {"+": add, "-": sub, "/": div, "*": mul}
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
 
-for operator in operations:
-    print(f"{operator}")
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    num2 = float(input("What's the second number?: "))
 
-operation = input("Pick an operation from the line above: ")
-answer = operations[operation](num1, num2)
-print(f"{num1} {operation} {num2} = {answer}")
+    for operator in operations:
+        print(f"{operator}")
+
+    operator = input("Pick an operation from the line above: ")
+    answer = operations[operator](num1, num2)
+    print(f"{num1} {operator} {num2} = {answer}")
+
+    calc_continue = True
+    while calc_continue:
+        continue_calc = input(
+            (f"Type 'y' to continue calculating with {answer} or 'n'  to exit: ")
+        )
+        if continue_calc == "y":
+            operator = input("Choose an operation: ")
+            num3 = float(input("Enter another number: "))
+            answer = operations[operator](answer, num3)
+            print(f"{num1} {operator} {num3} = {answer}")
+        elif continue_calc == "n":
+            clear()
+            calculator()
+
+
+calculator()
